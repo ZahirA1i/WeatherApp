79bbe0ab1fff62bd83a6fd81755213241cd3e20f
Zahir Ali <zahirali@Zahirs-Air.vc.shawcable.net>
2023-01-23 17:22:36 -0800

    finished main input & output of data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,41 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 export default function Home() {
+  const [data, setData] = useState({})
+  const [location,setLocation] = useState('');
+  const [weather, setWeather] = useState('')
+  const [errorMessage, setErrorMessage] = useState('');
+
+
+  var apiKey = "5dfcc4ac8bc24858dc36669d2e82db95"
+  var lang = "en"
+  var units = "metric"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=${apiKey}&lang=${lang}`
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter') {
+      axios.get(url)
+        .then((response) => {
+          console.clear();
+          setData(response.data)
+          console.log(response.data)
+          setWeather(response.data.weather)
+          setErrorMessage("")
+        }).catch(err => {
+          console.log(err)
+          setErrorMessage("Please enter another location")
+          setData({})
+          setWeather()
+        })
+        setLocation('')
+    }
+  }
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +45,32 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
+      {errorMessage}
+
+        <input
         
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        placeholder='Enter Location'
+        onKeyDown={searchLocation}
+        type="text"
+        
+        />
+        {data.name}
+
+
+        {
+          weather && weather.map((w, index) => {
+            return(
+              <div key={index}>
+                <div>{w.description}</div>
+                <div>{w.main}</div>
+              </div>
+            )
+          })
+        }
+
       </main>
 
   
